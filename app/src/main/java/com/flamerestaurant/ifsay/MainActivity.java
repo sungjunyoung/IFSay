d39223f3d26c9cd9dad748d21470d3ed56b7c9fe
Eunbeom <sst0424@nate.com>
2016-05-16 01:20:51 +0900

    unused import 제거, 미사용 코드 주석처리

--- app/src/main/java/com/flamerestaurant/ifsay/MainActivity.java
+++ app/src/main/java/com/flamerestaurant/ifsay/MainActivity.java
@@ -1,23 +1,17 @@
 package com.flamerestaurant.ifsay;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
 
 public class MainActivity extends Activity {
 
-    @BindView(R.id.text)
-    TextView textView;
+//    @BindView(R.id.text)
+//    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
+//        ButterKnife.bind(this);
     }
 }
--- app/src/main/java/com/flamerestaurant/ifsay/SplashActivity.java
+++ app/src/main/java/com/flamerestaurant/ifsay/SplashActivity.java
@@ -2,9 +2,8 @@ package com.flamerestaurant.ifsay;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends Activity {
 

9885ddd1ac06eaa6c47c730c23e1a296e12a94a7
wnsdud1861 <wnsdud1861@gmail.com>
2016-05-22 02:07:38 +0900

    no message

--- app/src/main/java/com/flamerestaurant/ifsay/SplashActivity.java
+++ app/src/main/java/com/flamerestaurant/ifsay/SplashActivity.java
@@ -53,7 +53,7 @@ public class SplashActivity extends Activity {
         realm.clear(Comment.class);
 
         RealmList<Question> questionList = new RealmList<Question>();
-        Question question1 = new Question(5,"어린 시절에서, 단 하나만 바꿀 수 있다면, 무엇을 바꾸겠는가?",new Date(2016,5,28));
+        Question question1 = new Question(0,"어린 시절에서, 단 하나만 바꿀 수 있다면, 무엇을 바꾸겠는가?",new Date(2016,5,28));
         Question question2 = new Question(1,"어제 봤는데, 오늘도 보고싶고, 내일도 보고싶은 인생의 영화는?",new Date(2016,5,19));
         Question question3 = new Question(2,"당신의 자서전 첫 문장은 어떻게 시작할것인가?",new Date(2016,5,20));
         Question question4 = new Question(3,"오늘 10억원이 생겼다. 무엇을 하겠는가?",new Date(2016,5,21));
@@ -64,7 +64,7 @@ public class SplashActivity extends Activity {
 
         RealmList<Ifsay> ifsayList = new RealmList<Ifsay>();
         //ifsayId, questionId, writer, content, ifsayCount, date, rgb
-        Ifsay ifsay1 = new Ifsay(9,5,"은범","피아노 학원을 끝까지 다닐거야. 어릴때는 왜 그렇게 도망갔을까? 여자친구에게 피아노 치며 노래불러주고 싶다. ",6,new Date(2016,5,28),"#ffffff");
+        Ifsay ifsay1 = new Ifsay(0,5,"은범","피아노 학원을 끝까지 다닐거야. 어릴때는 왜 그렇게 도망갔을까? 여자친구에게 피아노 치며 노래불러주고 싶다. ",6,new Date(2016,5,28),"#ffffff");
         Ifsay ifsay2 = new Ifsay(1,1,"결","당연히 '이터널 선샤인'. 잊었다고 생각한 그 사람을 자꾸 떠오르게하지만, 어쩔수 없이 보게된다. 재개봉했다던데 ㅜㅜ",13,new Date(2016,5,19),"#ffffff");
         Ifsay ifsay3 = new Ifsay(2,1,"준영","비긴어게인이요! 생각난김에 다운받아 봐야겠어요..",10,new Date(2016,05,19),"#ffffff");
         Ifsay ifsay4 = new Ifsay(3,2,"준영","그 누구도 22년뒤 내가  병원에서 꼼짝없이 누워있을 것을 알지 못했다.",22,new Date(2016,5,20),"#ffffff");
@@ -73,9 +73,9 @@ public class SplashActivity extends Activity {
         Ifsay ifsay7 = new Ifsay(6,4,"은범","끊어놓고 세번 갔던 PT를 끝까지 갔을꺼다. 돈도 돈이지만, 그때 운동했다면 ㅜ.ㅜ 술 조금만 덜먹고 더 뛰었다면.. 퇴원하면 꼭 다시 운동 열심히 할거다.",77,new Date(2016,5,22),"#ffffff");
         Ifsay ifsay8 = new Ifsay(7,4,"예진","저는 소설쓰는걸 배울거에요. 병원에 와서 책을 많이 읽고 써보기도하는데, 어떻게 시작해야할지 모르겠어요. 소설쓰는 법을 배우고 여행을 많이 다니면서 좋은 책을 쓰고싶어요",21,new Date(2016,5,22),"#ffffff");
         Ifsay ifsay9 = new Ifsay(8,4,"준영","회사 영업왕 실적을 꼭 찍을거에요. 힘들다고 욕하는 회사지만 사실 정말 재미있게 다녔거든요.. 선배들한테 배운걸 써먹고, 제 한계를 시험하고 싶어요",45,new Date(2016,5,22),"#ffffff");
-        Ifsay ifsay10 = new Ifsay(10,5,"예진","보약을 매일매일 챙겨먹을꺼야",6,new Date(2016,5,28),"#ffffff");
-        Ifsay ifsay11 = new Ifsay(11,5,"결","짝사랑하던 단짝친구에게 고백할꺼야",13,new Date(2016,5,28),"#ffffff");
-        Ifsay ifsay12 = new Ifsay(12,2,"은범","그러지 말았어야 했다.",27,new Date(2016,5,20),"#ffffff");
+        Ifsay ifsay10 = new Ifsay(9,5,"예진","보약을 매일매일 챙겨먹을꺼야",6,new Date(2016,5,28),"#ffffff");
+        Ifsay ifsay11 = new Ifsay(10,5,"결","짝사랑하던 단짝친구에게 고백할꺼야",13,new Date(2016,5,28),"#ffffff");
+        Ifsay ifsay12 = new Ifsay(11,2,"은범","그러지 말았어야 했다.",27,new Date(2016,5,20),"#ffffff");
         ifsayList.add(ifsay1); ifsayList.add(ifsay2);
         ifsayList.add(ifsay3); ifsayList.add(ifsay4);
         ifsayList.add(ifsay5); ifsayList.add(ifsay6);
@@ -84,7 +84,7 @@ public class SplashActivity extends Activity {
         ifsayList.add(ifsay11); ifsayList.add(ifsay12);
 
         RealmList<Comment> commentList = new RealmList<Comment>();
-        Comment comment1 = new Comment(12,2,"저도 비긴어게인 정말 좋아하는데","병훈",new Date(2016,5,19));
+        Comment comment1 = new Comment(0,2,"저도 비긴어게인 정말 좋아하는데","병훈",new Date(2016,5,19));
         Comment comment2 = new Comment(1,4,"저도 지긋지긋한 휠체어 벗어나고 싶어요!","은범",new Date(2016,5,20));
         Comment comment3 = new Comment(2,6,"맞아요.. 운동 안한건 정말 후회되어요. 엄마랑 산책이라도 많이 갈껄...\n","결",new Date(2016,5,22));
         Comment comment4 = new Comment(3,6,"222 운동의 중요성은 정말.. 퇴원하면 누구보다 열심히 몸 관리할거에요","준영",new Date(2016,5,22));

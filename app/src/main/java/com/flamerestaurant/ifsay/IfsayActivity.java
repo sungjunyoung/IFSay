4e185e8c6d986bd6905cb16c80f30d906d8f5c94
Eunbeom <sst0424@nate.com>
2016-05-21 20:32:42 +0900

    ifsay ViewPager 추가

--- app/src/main/java/com/flamerestaurant/ifsay/IfsayActivity.java
+++ app/src/main/java/com/flamerestaurant/ifsay/IfsayActivity.java
@@ -2,14 +2,49 @@ package com.flamerestaurant.ifsay;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class IfsayActivity extends Activity {
 
-    private String ifsay_content;
+    private ViewPager pager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ifsay);
+
+        pager = (ViewPager) findViewById(R.id.ifsay_pager);
+        pager.setAdapter(new Adapter());
+    }
+
+    private class Adapter extends PagerAdapter {
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            View view = getLayoutInflater().inflate(R.layout.page_ifsay, container, false);
+            TextView textView = (TextView) view.findViewById(R.id.position_for_debug);
+            textView.setText(String.valueOf(position));
+            container.addView(view);
+            return view;
+        }
+
+        @Override
+        public int getCount() {
+            return 10;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
+        }
     }
 }

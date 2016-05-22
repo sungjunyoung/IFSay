c28baedac07f7cf0335bc35e7f461418b3ac4220
wnsdud1861 <wnsdud1861@gmail.com>
2016-05-22 09:00:30 +0900

    ㅅㄹ려주세요

--- app/src/main/java/com/flamerestaurant/ifsay/QuestionActivity.java
+++ app/src/main/java/com/flamerestaurant/ifsay/QuestionActivity.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.flamerestaurant.ifsay.hue.HueManager;
